46f39f4f1af0d384427488caefcf08dc473319fb
MegaPinata <MegaPinata@users.noreply.github.com>
2022-07-01 19:22:02 -0400

    Update prog1New.lua

--- prog1New.lua
+++ prog1New.lua
@@ -5,67 +5,94 @@ local drawerController = peripheral.wrap("functionalstorage:storage_controller_0
 local mobs = {}
 
 function genFunction()
-    local func = function(redstoneIntegrator, face)
-        peripheral.call(redstoneIntegrator, "setOutput", face, not peripheral.call(redstoneIntegrator, "getOutput", face))
-    end
-    return func
+	local func = function(redstoneIntegrator, face)
+		peripheral.call(
+			redstoneIntegrator,
+			"setOutput",
+			face,
+			not peripheral.call(redstoneIntegrator, "getOutput", face)
+		)
+	end
+	return func
 end
 
 function checkItemLevels(storageController, mobs, t)
 	for _, mob in ipairs(mobs) do
-        for _, drop in ipairs(mob.dropsWanted) do
-            local item = drawerController.getItemDetail(item.inventorySlot)
-            if item.count <= drop.quantity and not t:getActive(mob.buttonName) then
-                t:onClick(mob.buttonName)
-            elseif item.count > drop.quantity and t:getActive(mob.buttonName) then
-                t:onClick(mob.buttonName)
-            end
-        end
-    end
+		for _, drop in ipairs(mob.dropsWanted) do
+			local item = drawerController.getItemDetail(item.inventorySlot)
+			if item.count <= drop.quantity and not t:getActive(mob.buttonName) then
+				t:onClick(mob.buttonName)
+			elseif item.count > drop.quantity and t:getActive(mob.buttonName) then
+				t:onClick(mob.buttonName)
+			end
+		end
+	end
 end
 
-table.insert(mobs, mobApi.Mob:new("Blaze", "Blaze", "redstoneIntegrator_6", {mobApi.Drop:new("minecraft:blaze_rod", 0, 3000)}))
-table.insert(mobs, mobApi.Mob:new("Wither Skeleton", "W Skel", "redstoneIntegrator_7", {mobApi.Drop:new("minecraft:wither_skeleton_skull", 0, 100)}))
-table.insert(mobs, mobApi.Mob:new("Enderman", "Ender", "redstoneIntegrator_8", {mobApi.Drop:new("minecraft:ender_pearl", 0, 5000)}))
-table.insert(mobs, mobApi.Mob:new("Phantom", "Phantom", "redstoneIntegrator_9", {mobApi.Drop:new("minecraft:phantom_membrane", 0, 3000 )}))
+table.insert(
+	mobs,
+	mobApi.Mob:new("Blaze", "Blaze", "redstoneIntegrator_6", { mobApi.Drop:new("minecraft:blaze_rod", 0, 3000) })
+)
+table.insert(
+	mobs,
+	mobApi.Mob:new(
+		"Wither Skeleton",
+		"W Skel",
+		"redstoneIntegrator_7",
+		{ mobApi.Drop:new("minecraft:wither_skeleton_skull", 0, 100) }
+	)
+)
+table.insert(
+	mobs,
+	mobApi.Mob:new("Enderman", "Ender", "redstoneIntegrator_8", { mobApi.Drop:new("minecraft:ender_pearl", 0, 5000) })
+)
+table.insert(
+	mobs,
+	mobApi.Mob:new(
+		"Phantom",
+		"Phantom",
+		"redstoneIntegrator_9",
+		{ mobApi.Drop:new("minecraft:phantom_membrane", 0, 3000) }
+	)
+)
 
 -- Find the slot the wanted items are in and store it
 for slot, item in pairs(drawerController.list()) do
 	for _, mob in ipairs(mobs) do
-        for _, drop in ipairs(mob.dropsWanted) do
-            if item.name == drop.name then
-                drop.inventorySlot = slot
-            end
-        end
+		for _, drop in ipairs(mob.dropsWanted) do
+			if item.name == drop.name then
+				drop.inventorySlot = slot
+			end
+		end
 	end
 end
 
 for _, mob in ipairs(mobs) do
-    peripheral.call(mob.redstoneIntegrator, "setOutput", "front", false)
+	peripheral.call(mob.redstoneIntegrator, "setOutput", "front", false)
 end
 
-for _, face in ipairs({"bottom","top","left","right","front"}) do
-    peripheral.call("redstoneIntegrator_14", "setOutput", face, false)
+for _, face in ipairs({ "bottom", "top", "left", "right", "front" }) do
+	peripheral.call("redstoneIntegrator_14", "setOutput", face, false)
 end
 
 local t = buttonApi.new("top")
 
-t:add(mobs[1].buttonName, genFunction(), 2, 2, 10, 4)
-t:add(mobs[2].buttonName, genFunction(), 2, 6, 10, 8)
-t:add(mobs[3].buttonName, genFunction(), 2, 10, 10, 12)
-t:add(mobs[4].buttonName, genFunction(), 2, 14, 10, 16)
+t:add(mobs[1].buttonName, genFunction(), 2, 2, 10, 4, true)
+t:add(mobs[2].buttonName, genFunction(), 2, 6, 10, 8, true)
+t:add(mobs[3].buttonName, genFunction(), 2, 10, 10, 12, true)
+t:add(mobs[4].buttonName, genFunction(), 2, 14, 10, 16, true)
 t:add("MSF", genFunction(), 32, 6, 38, 8)
 t:add("Mash", genFunction(), 24, 2, 30, 4)
 t:add("Fans", genFunction(), 32, 2, 38, 4)
 t:add("ESpwn", genFunction(), 24, 14, 30, 16)
-t:add("Light", genFunction(), 32, 14, 38, 16)
+t:add("Light", genFunction(), 32, 14, 38, 16, true)
 
 for _, button in ipairs(t) do
-    local state = false
+	local state = false
 	if value[3] == "front" then
-        state = true
+		state = true
 	end
-    peripheral.call(button[2], "setOutput", button[3], state)
+	peripheral.call(button[2], "setOutput", button[3], state)
 end
 
 t:draw()
@@ -75,32 +102,34 @@ t:draw()
 while true do
 	local event, p1 = t:handleEvents(os.pullEvent())
 	if event == "button_click" then
-        local found = false
+		local found = false
 
-        for _, mob in ipairs(mobs) do
-            if mob.buttonName == p1 then
-                found = true
-                t:onClick(mob.buttonName, mob.redstoneIntegrator, "front")
-                break
-            end
-        end
-        if not found then
-            if p1 == "MSF" then
-                t:onClick(p1, "redstoneIntegrator_14", "bottom")
-            elseif p1 == "Mash" then
-                t:onClick(p1, "redstoneIntegrator_14", "top")
-            elseif p1 == "Fans" then
-                t:onClick(p1, "redstoneIntegrator_14", "left")
-            elseif p1 == "Espwn" then
-                t:onClick(p1, "redstoneIntegrator_14", "right")
-            else if p1 == "Light" then
-                t:onClick(p1, "redstoneIntegrator_14", "front")
-            end
-        end
+		for _, mob in ipairs(mobs) do
+			if mob.buttonName == p1 then
+				found = true
+				t:onClick(mob.buttonName, mob.redstoneIntegrator, "front")
+				break
+			end
+		end
+
+		if not found then
+			if p1 == "MSF" then
+				t:onClick(p1, "redstoneIntegrator_14", "bottom")
+			elseif p1 == "Mash" then
+				t:onClick(p1, "redstoneIntegrator_14", "top")
+			elseif p1 == "Fans" then
+				t:onClick(p1, "redstoneIntegrator_14", "left")
+			elseif p1 == "Espwn" then
+				t:onClick(p1, "redstoneIntegrator_14", "right")
+			else
+				if p1 == "Light" then
+					t:onClick(p1, "redstoneIntegrator_14", "front")
+				end
+			end
+		end
+
+		if event == "timer" then
+			checkItemLevels(drawerController, itemsWanted, buttonTable, t)
+		end
 	end
-
-	if event == "timer" then
-		checkItemLevels(drawerController, itemsWanted, buttonTable, t)
-    end
 end
-	end
